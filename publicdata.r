5abe4e9365d616677e8f2bdf235295a94a612867
Bolaji25 <45816373+Bolaji25@users.noreply.github.com>
2018-12-13 10:09:45 +0100

    Update publicdata.r
    new ver

--- publicdata.r
+++ publicdata.r
@@ -1,45 +1,33 @@
-library(gdata)
-library(GenomicRanges)
 library(BSgenome.Celegans.UCSC.ce11)
 library(rtracklayer)
-library(ggbio)
 
-
-# get rex sites from Albritton paper
-#Albritton_elife2017="https://elifesciences.org/download/aHR0cHM6Ly9jZG4uZWxpZmVzY2llbmNlcy5vcmcvYXJ0aWNsZXMvMjM2NDUvZWxpZmUtMjM2NDUtc3VwcDEtdjEuemlw/elife-23645-supp1-v1.zip?_hash=8w773VoPpu6mx1PUQppgaFNrEniZQDyGrnfBDR6QDt8%3D"
-#download.file(Albritton_elife2017,destfile="./Albritton_elife2017.zip")
-#unzip("./Albritton_elife2017.zip",files="Tables/SupplementaryFile1D.xls",junkpaths=T)
-#rexSites<-read.xls("./SupplementaryFile1D.xls")
-
-#read in the ahringer files' table
-ahringerfiles<-read.table("./ahringer.txt", header=TRUE, stringsAsFactors = FALSE)
-ahringerfiles
-for (i in 1:dim(ahringerfiles)[1]) {
-download.file(ahringerfiles$Sample[i],destfile=paste0(ahringerfiles$FileName[i],".bw"))
+#read in the public files' table, always set header so that the header is defined
+publicfiles<-read.table("./publicfiles.txt", header=TRUE, stringsAsFactors = FALSE)
+publicfiles
+for (i in 1:dim(publicfiles)[1]) {
+download.file(publicfiles$Sample[i],destfile=paste0(publicfiles$FileName[i],"_ce10.bw"))
 }
+#import the bigwig files
+pub<-import("atac_wild_l3_ce10.bw")
+pub<-import(paste0(publicfiles$FileName[i],"_ce10.bw"))
+pub
 
-# change chr names to UCSC formats
-rexSites$Chromosome<-gsub("CHROMOSOME_","chr",rexSites$Chromosome)
+#for the loops
+for (i in 1:dim(publicfiles)[1]) {
+pub<-import(paste0(publicfiles$FileName[i],"_ce10.bw"))
 
-# make Granges
-rexGR<-GRanges(seqnames=rexSites$Chromosome,ranges=IRanges(start=rexSites$Start,end=rexSites$End),
-               strand="*", rexSites[,c("Rank","Previous.Name","strength.category")])
 
-# get liftover chain file from UCSC
-download.file("http://hgdownload.cse.ucsc.edu/goldenPath/ce10/liftOver/ce10ToCe11.over.chain.gz",
-              destfile="./ce10ToCe11.over.chain.gz")
-system("gunzip ./ce10ToCe11.over.chain.gz")
+#import the downloaded chain file
 chainFile<-import.chain("./ce10ToCe11.over.chain")
 
-#lift rex sites from ce10 to ce11
-rexGR_ce11<-unlist(liftOver(rexGR,chain=chainFile))
-# you need to make sure all chromosomes are present in the seqlevels and seqlengths (get from BSgenome)
-seqlevels(rexGR_ce11)<-seqlevels(Celegans)
-seqlengths(rexGR_ce11)<-seqlengths(Celegans)
+#lift the files from ce10 to ce11
+pub_ce11<-unlist(liftOver(pub, chain = chainFile))
+#to make sure all chromosomes are present in the seqlevels and seqlengths (get from BSgenome)
+seqlevels(pub_ce11)<-seqlevels(Celegans)
+seqlengths(pub_ce11)<-seqlengths(Celegans)
 
-rexGR_ce11$strength.category<-factor(rexGR_ce11$strength.category,levels=c("strong","intermediate","weak"))
 
-# plot rex sites on chromosome layout
-autoplot(rexGR_ce11,layout="karyogram",aes(color = strength.category,fill = strength.category)) +
-  theme_alignment() +
-  ggtitle("Strength of rex sites")
+#to export the files
+export(pub_ce11, paste0(publicfiles$FileName[i],"_ce11.gtf"),  "gtf")
+export(pub_ce11, paste0(publicfiles$FileName[i], "_ce11.bw"), "bw")
+}
